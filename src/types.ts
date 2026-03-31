eb1ed027ca1aeb2761cb2af6140ec3c3a4569f50
Rafał Myśliwczyk <mysliwczykrafal@gmail.com>
2026-03-31 22:15:29 +0200

    Form validation and summary improvements
    - Fixed whitespace being allowed in fields accepting text
    - Added min year of vehicle production to be the first car produced
    - Deleted vehicle year from summary where not applicable

--- src/types.ts
+++ src/types.ts
@@ -25,17 +25,17 @@ export enum RiskLevel {
 
 export const schema = z
 	.object({
-		firstName: z.string().min(1, 'Please provide the first name'),
-		lastName: z.string().min(1, 'Please provide the last name'),
+		firstName: z.string().trim().min(1, 'Please provide the first name'),
+		lastName: z.string().trim().min(1, 'Please provide the last name'),
 		age: z.coerce
 			.number<number>('Age must be a number')
 			.gte(18, 'Age must be greater or equal 18')
 			.lte(100, 'Age must be less than or equal 100'),
-		city: z.string().min(1, 'Please provide the city'),
+		city: z.string().trim().min(1, 'Please provide the city'),
 		insuranceType: z.literal(Object.values(InsuranceType)),
-		vehicleProductionYear: z.coerce.number<number>(
-			'You can use only numbers'
-		),
+		vehicleProductionYear: z.coerce
+			.number<number>('You can use only numbers')
+			.optional(),
 		coverageAmount: z.coerce
 			.number<number>('You can use only numbers')
 			.gte(1000, 'Coverage must by greater or equal 1000'),
@@ -44,12 +44,15 @@ export const schema = z
 	.superRefine((data, ctx) => {
 		if (
 			data.insuranceType == InsuranceType.Car &&
-			data.vehicleProductionYear <= 0
+			data.vehicleProductionYear &&
+			data.vehicleProductionYear <= 1886
 		) {
 			ctx.addIssue({
 				code: 'custom',
 				path: ['vehicleProductionYear'],
-				message: 'Please provide a valid vehicle production year',
+				message:
+					'Please provide a valid vehicle production year. First car ever produced was 1886.',
+				// https://en.wikipedia.org/wiki/Benz_Patent-Motorwagen - Year of the first car
 			})
 		}
 	})
